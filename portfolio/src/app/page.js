48f042a87c5c4f43a792d36d5fa2c2ea9b330b7e
ConnorHoare <connor@tipcigroup.com>
2023-06-24 00:44:43 +0100

    nav update

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,8 +1,10 @@
+'use client'
+import { useState } from 'react';
 import Image from 'next/image'
 import Link from 'next/link'
+import { Navbar } from '../../components'
 
 export default function Home() {
-  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
