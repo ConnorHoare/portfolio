348daf032d142ccbca6b57153e6472eaa9e200ab
ConnorHoare <connor@tipcigroup.com>
2023-06-30 09:38:49 +0100

    hero added

--- portfolio/src/app/case-studies/[slug]/page.jsx
+++ portfolio/src/app/case-studies/[slug]/page.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import Image from 'next/image'
+
+import { CarRentDesktop, CarRentMobile, lineDecoration } from '../../../../assets'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex justify-center'>
+      <div className='flex flex-col'>
+        <div>
+          <p>WEB DEV PROJECT</p>
+        </div>
+        <div>
+          <p>MORENT - A Car Rental Project</p>
+        </div>
+        <div className='flex'>
+          <Image src={CarRentDesktop} />
+          <Image src={CarRentMobile} />
+        </div>
+        <div className='flex'>
+          <div className='flex'>
+            <p>Demo Site</p>
+            <Image src={lineDecoration} />
+          </div>
+          <div className='flex'>
+            <p>Source Code</p>
+            <Image src={lineDecoration} />
+          </div>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
