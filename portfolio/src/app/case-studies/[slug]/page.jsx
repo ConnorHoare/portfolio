2ad42bed91285511dabeb0563b6dfcaebdc25951
ConnorHoare <connor@tipcigroup.com>
2023-07-03 13:51:03 +0100

    mobile responsive update

--- portfolio/src/app/case-studies/[slug]/page.jsx
+++ portfolio/src/app/case-studies/[slug]/page.jsx
@@ -44,9 +44,9 @@ const page = () => {
           <div className='text-center'>
             <p className='font-bold text-6xl text-blue-1 dark:text-blue-4'>MORENT - A Car Rental Project</p>
           </div>
-          <div className='flex items-center justify-around'>
-            <Image src={CarRentDesktop} />
-            <Image src={CarRentMobFull} />
+          <div className='md:flex items-center justify-around'>
+            <Image src={CarRentDesktop} className='flex justify-center mx-auto md:mx-0'/>
+            <Image src={CarRentMobFull} className='hidden md:flex'/>
           </div>
           <div className='flex justify-around'>
             <div className='flex gap-4'>
@@ -78,23 +78,23 @@ const page = () => {
         </div>
       </div>
 
-      <div className='flex bg-grey-6 justify-around p-10 dark:bg-grey-5'>
-        <div className='flex-col space-y-3'>
+      <div className='md:flex-row flex flex-col bg-grey-6 justify-around p-10 dark:bg-grey-5 space-y-4 md:space-y-0'>
+        <div className='flex-col md:space-y-3'>
           <p className='font-bold text-black-1 text-sm dark:text-white'>MY ROLE</p>
           <p className='font-bold text-grey-3 text-2xl'>Software Engineer</p>
         </div>
-        <div className='flex-col space-y-3'>
+        <div className='flex-col md:space-y-3'>
           <p className='font-bold text-black-1 text-sm dark:text-white'>START DATE</p>
           <p className='font-bold text-grey-3 text-2xl'>24/01/2023</p>
         </div>
-        <div className='flex-col space-y-3'>
+        <div className='flex-col md:space-y-3'>
           <p className='font-bold text-black-1 text-sm dark:text-white'>END DATE</p>
           <p className='font-bold text-grey-3 text-2xl'>02/03/2023</p>
         </div>
       </div>
 
       <div className='flex justify-around'>
-        <div className='flex-col p-24 space-y-8'>
+        <div className='flex-col md:p-24 p-10 space-y-8'>
           <div className='flex-col space-y-2'>
             <p className='font-bold text-blue-1 text-sm'>TECHNOLOGIES USED</p>
             <p className='font-bold text-4xl'>Tech Stack</p>
@@ -110,7 +110,7 @@ const page = () => {
       </div>
 
       <div className='flex justify-center bg-grey-6 dark:bg-grey-5'>
-        <div className='space-y-5 py-24 px-48 text-4xl text-blue-2 dark:text-grey-3'>
+        <div className='space-y-5 md:py-24 md:px-48 px-10  py-16 text-4xl text-blue-2 dark:text-grey-3'>
           <p>
             Morent, a cutting-edge car rental application that was developed using the MERN stack. The app boasts a dynamic homepage that showcases featured vehicles, along with convenitent pickup and drop-off location options and a comprehensive list of available cars.
           </p>
@@ -123,7 +123,7 @@ const page = () => {
         </div>
       </div>
 
-      <div className='flex justify-center p-44'>
+      <div className='flex justify-center md:p-44 p-10'>
         <div className='flex-col space-y-10'>
           <div className='flex-col space-y-11 border-blue-1 border p-16 dark:border-blue-4'>
             <div className=''>
@@ -153,52 +153,52 @@ const page = () => {
         </div>
       </div>
 
-      <div className='flex flex-col w-full py-24 px-24'>
+      <div className='flex flex-col w-full md:py-24 md:px-24 p-10'>
         <div className='pb-12'>
           <p className='text-center font-bold text-3xl'>My Process</p>
         </div>
-        <div className='flex flex-row justify-around '>
-          <div className='flex-col space-y-4'>
+        <div className='flex md:flex-row flex-col md:space-y-0 space-y-8 justify-around '>
+          <div className='flex-col space-y-4 mx-auto md:mx-0'>
             {isDark ? (
               <Image src={DarkDefinitionIcon} />
             ) : (
               <Image src={DefinitionIcon} />
             )}
 
-            <p className='text-center font-bold dark:text-white'>Definition</p>
+            <p className='text-center font-bold dark:text-white md:text-base text-xl'>Definition</p>
           </div>
-          <div className='flex-col space-y-4'>
+          <div className='flex-col space-y-4 mx-auto md:mx-0'>
             {isDark ? (
               <Image src={DarkPlanningIcon} />
             ) : (
               <Image src={PlanningIcon} />
             )}
 
-            <p className='text-center font-bold dark:text-white'>Planning</p>
+            <p className='text-center font-bold dark:text-white md:text-base text-xl'>Planning</p>
           </div>
-          <div className='flex-col space-y-4'>
+          <div className='flex-col space-y-4 mx-auto md:mx-0'>
             {isDark ? (
               <Image src={DarkDevelopmentIcon} />
             ) : (
               <Image src={DevlopmentIcon} />
             )}
 
-            <p className='text-center font-bold dark:text-white'>Development</p>
+            <p className='text-center font-bold dark:text-white md:text-base text-xl'>Development</p>
           </div>
-          <div className='flex-col space-y-4'>
+          <div className='flex-col space-y-4 mx-auto md:mx-0'>
             {isDark ? (
               <Image src={DarkDeliveryIcon} />
             ) : (
               <Image src={DeliveryIcon} />
             )}
 
-            <p className='text-center font-bold dark:text-white'>Delivery</p>
+            <p className='text-center font-bold dark:text-white md:text-base text-xl'>Delivery</p>
           </div>
         </div>
       </div>
 
-      <div className='flex flex-col px-24 py-12 space-y-8'>
-        <div className='flex-col w-1/2 pl-6'>
+      <div className='flex flex-col md:px-24 md:py-12 space-y-8 '>
+        <div className='flex-col md:w-1/2 w-full px-6'>
           {isDark ? (
             <Image src={DarkUserFlowIcon} height={75} width={75} />
           ) : (
@@ -222,8 +222,8 @@ const page = () => {
 
       <div className='flex justify-around items-center bg-grey-6 dark:bg-grey-5 py-12'>
         <div className='flex flex-col w-full'>
-          <div className='text-left w-full self-start px-36'><p className=' text-sm font-bold text-blue-1 dark:text-blue-4'>IMAGES</p></div>
-          <div className='text-left w-full px-36'><p className=' text-4xl font-bold dark:text-white'>Product</p></div>
+          <div className='text-left w-full self-start md:px-36 px-10'><p className=' text-sm font-bold text-blue-1 dark:text-blue-4'>IMAGES</p></div>
+          <div className='text-left w-full md:px-36 px-10'><p className=' text-4xl font-bold dark:text-white'>Product</p></div>
           <div className='flex justify-center w-full py-12'>
             <div className='flex-col'>
               <div className='flex flex-row justify-center items-center'>
@@ -231,7 +231,7 @@ const page = () => {
                   <Image src={CarRentDesktop} />
                 </div>
                 <div>
-                  <Image src={CarRentMobFull} />
+                  <Image src={CarRentMobFull} className='hidden md:flex'/>
                 </div>
               </div>
               <div className='text-center'> ... </div>
@@ -254,10 +254,10 @@ const page = () => {
 
       <div className='flex justify-around items-center py-12 dark:bg-black-1'>
         <div className='flex flex-col w-full'>
-          <div className='text-left w-full self-start px-36'><p className=' text-sm font-bold text-blue-1 dark:text-blue-4'>LEARNINGS & CHALLENGES</p></div>
-          <div className='text-left w-full px-36'><p className=' text-4xl font-bold dark:text-white'>Takeaways</p></div>
+          <div className='text-left w-full self-start md:px-36 px-10'><p className=' text-sm font-bold text-blue-1 dark:text-blue-4'>LEARNINGS & CHALLENGES</p></div>
+          <div className='text-left w-full md:px-36 px-10'><p className=' text-4xl font-bold dark:text-white'>Takeaways</p></div>
 
-          <div className='mt-12 flex flex-col bg-grey-6 rounded mx-36 p-12 space-y-4 dark:bg-grey-5'>
+          <div className='mt-12 flex flex-col bg-grey-6 rounded md:mx-36 p-12 space-y-4 dark:bg-grey-5'>
             <p className='font-bold text-green-2'>LEARNINGS</p>
             <div className='flex flex-row items-center gap-2'>
               <Image src={TickIcon} />
@@ -277,7 +277,7 @@ const page = () => {
             </div>
           </div>
 
-          <div className='mt-12 flex flex-col bg-grey-6 rounded mx-36 p-12 space-y-4 dark:bg-grey-5'>
+          <div className='mt-12 flex flex-col bg-grey-6 rounded md:mx-36 p-12 space-y-4 dark:bg-grey-5'>
             <p className='font-bold text-red-1'>CHALLENGES</p>
             <div className='flex flex-row items-center gap-2'>
               <Image src={BullseyeIcon} />
@@ -297,12 +297,12 @@ const page = () => {
 
       <div className='flex justify-around items-center py-12 dark:bg-grey-5'>
         <div className='flex flex-col w-full'>
-          <div className='text-left w-full self-start px-36'><p className=' text-sm font-bold text-blue-1 dark:text-blue-4'>PROJECTS</p></div>
-          <div className='text-left w-full px-36'><p className=' text-4xl font-bold'>Other Case Studies</p></div>
-          <div className='flex flex-row justify-between px-36 py-8'>
+          <div className='text-left w-full self-start md:px-36 px-10'><p className=' text-sm font-bold text-blue-1 dark:text-blue-4'>PROJECTS</p></div>
+          <div className='text-left w-full md:px-36 px-10'><p className=' text-4xl font-bold'>Other Case Studies</p></div>
+          <div className='flex md:flex-row flex-col md:justify-between md:px-36 px-10 py-8 space-y-8 md:space-y-0'>
 
-            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white flex flex-col dark:bg-grey-7 pb-4">
-              <div className="h-64 relative flex-grow">
+            <div className="md:max-w-sm w-full rounded overflow-hidden shadow-lg bg-white flex flex-col dark:bg-grey-7 pb-4">
+              <div className="md:h-64 relative flex-grow">
                 <Image
                   src={JobitFigma}
                   style={{ objectFit: 'cover' }}
@@ -318,8 +318,8 @@ const page = () => {
               </div>
             </div>
 
-            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white flex flex-col dark:bg-grey-7 pb-4">
-              <div className="h-64 relative flex-grow">
+            <div className="md:max-w-sm w-full rounded overflow-hidden shadow-lg bg-white flex flex-col dark:bg-grey-7 pb-4">
+              <div className="md:h-64 relative flex-grow">
                 <Image
                   src={JobitFigma}
                   style={{ objectFit: 'cover' }}
@@ -335,8 +335,8 @@ const page = () => {
               </div>
             </div>
 
-            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white flex flex-col dark:bg-grey-7 pb-4">
-              <div className="h-64 relative flex-grow">
+            <div className="md:max-w-sm w-full rounded overflow-hidden shadow-lg bg-white flex flex-col dark:bg-grey-7 pb-4">
+              <div className="md:h-64 relative flex-grow">
                 <Image
                   src={JobitFigma}
                   style={{ objectFit: 'cover' }}
